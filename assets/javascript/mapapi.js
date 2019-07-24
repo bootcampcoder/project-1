aa8be9f962068e27c38044eb2a8e97334d93c5fe
Ravi Joshi <ravipiyushjoshi@gmail.com>
2019-07-24 00:29:11 -0400

    Updated script for multiple markup displayed on the map

--- assets/javascript/mapapi.js
+++ assets/javascript/mapapi.js
@@ -9,7 +9,7 @@ var lng = [];
 
 function ajaxCall(){
    // var queryURL1 = "https://www.eventbriteapi.com/v3/categories/";
-   var queryURL1 = "https://www.eventbriteapi.com/v3/events/search/?location.address=toronto&location.within=10km&expand=venue&price=free";
+   var queryURL1 = "https://www.eventbriteapi.com/v3/events/search/?location.address=toronto&expand=venue&price=free";
    var settings1 = {
       async: true,
       crossDomain: true,
@@ -29,81 +29,127 @@ function ajaxCall(){
 
     eventObj.forEach(event => {
        eventName.push(event.name.text);
-        city.push(event.venue.address.city);
+        console.log(event.name.text); 
+       city.push(event.venue.address.city);
+        console.log(event.venue.address.city);
         lat.push(event.venue.latitude);
+        console.log(event.venue.latitude);
         lng.push(event.venue.longitude);
-
-        
-        
+        console.log(event.venue.longitude);
+         
+        console.log("Float Lattitide is:"+parseFloat(lat),"Float Lattitide is:"+parseFloat(lng)); 
+       
+        //initMap(parseInt(lat),parseInt(lng));
+        //initMap(parseInt(lat),parseInt(lng));
+        //  initMap();
+      // initMap(parseFloat(lat),parseFloat(lng));
     })
     console.log(city[0]);
-    console.log(lat[0]);
-    console.log(lng[0]);
+    console.log(parseFloat(lat[0]));
+    console.log(parseFloat(lng[0]));
     console.log(eventName[0]);
+    eqfeed_callback();
     });
     
 }
 //var longitude = [lat, lng];
 //console.log(longitude);
 
+
 ajaxCall();
 
-function initMap(){
-    var options = {
-        center: {lat:43.653226 , lng: -79.383},
-       // center:longitude,
-        zoom: 10
-    };
+//function initMap(){
+  //   debugger;  
+//     //var place = {lat: -25.344, lng: 131.036};
+//     var place = {lat: lat, lng: lng};
+//   // The map, centered at Uluru
+//     var map = new google.maps.Map(
+//       document.getElementById('map'), {zoom: 4, center: place});
+//   // The marker, positioned at Uluru
+//   var marker = new google.maps.Marker({position: place, map: map});
 
-    map = new google.maps.Map(document.getElementById("map"), options);
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 2,
+      center: new google.maps.LatLng(-25.344,131.036),
+      mapTypeId: 'terrain'
+     
+    });
+    
+    // Create a <script> tag and set the USGS URL as the source.
+   // var script = document.createElement('script');
+    // This example uses a local copy of the GeoJSON stored at
+    // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+   // script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+    //document.getElementsByTagName('head')[0].appendChild(script);
+  } 
 
-    var currentLocation = new google.maps.Marker({
-        position: options.center,
-        map: map
-    })
+  
+function eqfeed_callback() {
+      debugger;
+    for (var i = 0; i < eventName.length; i++) {
+        //var coords = results.features[i].geometry.coordinates;
+        var latLng = new google.maps.LatLng(lat[i],lng[i]);
+        var marker = new google.maps.Marker({
+          position: latLng,
+          map: map
+        });
+      }
+  }
+ 
 
-    var input = document.getElementById("search");
-    var searchBox = new google.maps.places.SearchBox(input);
+  // Loop through the results array and place a marker for each
+  // set of coordinates.
+  //window.eqfeed_callback();
+    
+  
 
-    map.addListener("bounds_changed", function(){
-        searchBox.setBounds(map.getBounds());
-    })
+
+
+
+    // var input = document.getElementById("search");
+    // var searchBox = new google.maps.places.SearchBox(input);
+
+    // map.addListener("bounds_changed", function(){
+    //     searchBox.setBounds(map.getBounds());
+    // })
 
     //get search results on the map
-    var markers = [];
+    //var markers = [];
 
-    searchBox.addListener("places_changed", function(){
-        var places = searchBox.getPlaces();
+    // searchBox.addListener("places_changed", function(){
+    //     var places = searchBox.getPlaces();
 
-        if(places.length === 0)
-        return;
+    //     if(places.length === 0)
+    //     return;
 
-        markers.forEach(function(m){
-            m.setMap(null);
-        });
-        markers = [];
+    //     markers.forEach(function(m){
+    //         m.setMap(null);
+    //     });
+    //     markers = [];
 
-        var bounds = new google.maps.LatLngBounds();
+    //     var bounds = new google.maps.LatLngBounds();
 
-        places.forEach(function(p){
-            if(!p.geometry)
-            return;
+    //     places.forEach(function(p){
+    //         if(!p.geometry)
+    //         return;
 
-            markers.push(new google.maps.Marker({
-                map: map,
-                title: p.name,
-                position: p.geometry.location
-            }));
+    //         markers.push(new google.maps.Marker({
+    //             map: map,
+    //             title: p.name,
+    //             position: p.geometry.location
+    //         }));
 
-            if(p.geometry.viewport)
-            bounds.union(p.geometry.viewport);
-            else
-            bounds.extend(p.geometry.location);
-        });
-        map.fitBounds(bounds);
-    })
+    //         if(p.geometry.viewport)
+    //         bounds.union(p.geometry.viewport);
+    //         else
+    //         bounds.extend(p.geometry.location);
+    //     });
+    //     map.fitBounds(bounds);
+   // })
 
-}
+//}
 
 
    
