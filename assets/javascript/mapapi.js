47073133c95d59843bd7691bba598917bc7804b6
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-07-26 13:46:19 -0400

    link adjusted again

--- assets/javascript/mapapi.js
+++ assets/javascript/mapapi.js
@@ -87,5 +87,5 @@ function eqfeed_callback() {
 }
 $(".search-btn").on("click", (e)=>{
   e.preventDefault();
-  window.location.href='../../searchResult.html';
+  window.location.href='https://bootcampcoder.github.io/project-1/searchResult.html';
 });
